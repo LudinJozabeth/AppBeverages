174eab06b995f75db2d34291c82b6ac1b92266db
Ludin Huerfano <lhuerfano@technisys.com>
2020-05-29 21:14:47 -0400

    Initial Commit

---
+++ AppBeverages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppBeverages
+//
+//  Created by Ludin Huerfano on 29-05-20.
+//  Copyright © 2020 Ludin Huerfano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
